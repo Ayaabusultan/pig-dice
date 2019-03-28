ac72a260912453fefe182cecf90ed6952bc5ad5e
ohood84 <obeidat.ohood@gmail.com>
2019-03-27 18:48:02 -0700

    it will add pigdice game

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,46 @@
+var index = 0;
+var rollsCounter=0
+var player1 = new Player();
+var player2 = new Player();
+
+
+var rollDice = function(){
+var diceValue= Math.floor(Math.random() * 6)+1;
+ rollsCounter++;
+ alert(diceValue);
+$("p").text(rollsCounter);
+ return diceValue;
+}
+
+function switchPlayer(){
+ if(index === 0){
+   index =1;
+ }
+ else{
+   index=0;
+ }
+}
+
+function Player(){
+ // this.playerName = name;
+ this.rollsCounter = 0;
+ this.current= 0;
+ this.score= 0;
+
+}
+
+$(document).ready(function(){
+
+
 function PigDice(){
-  this.players = [player1,player2];
+   // this.players = [player1,player2];
 }
 
 PigDice.prototype.addPlayer= function(player){
   this.players.push(player);
 }
 
-var rollsCounter=0
 
-Player.prototype.rollDice =function(){
-  var diceValue= Math.floor(Math.random() * 6)+1;
-  rollsCounter++;
-  return this.diceValue;
-
-}
-
-function switchPlayer(){
-  if(index === 0){
-    index =1;
-  }
-  else{
-    index=0;
-  }
-}
-
-function Player(){
-  // this.playerName = name;
-  this.rollsCounter = 0;
-  this.current= 0;
-  this.score= 0;
-
-}
 
 
 Player.prototype.countCurrent= function(diceValue){
@@ -51,18 +61,18 @@ Player.prototype.countScore= function(){
 
 
 
-$(document).ready(function(){
-  var index = 0;
+
+
   // var newPigDice = new  PigDice();
 
-  var player1 = new Player();
-  var player2 = new Player();
+
 
   function textResult(){
     $(".current1").text(player1.current);
     $(".current2").text(player2.current);
     $(".score1").text(player1.score);
     $(".score2").text(player2.score);
+
 
   }
 
